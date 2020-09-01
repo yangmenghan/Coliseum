2853681720ef7024477110ac0350af362778b26b
Menghan Yang <menghan.yang@360learning.com>
2020-09-01 22:25:08 +0200

    #12 - Simplify GameEndContract

--- app/src/main/java/com/windrose/coliseum/ludicoliseum/view/end/GameEndContract.kt
+++ app/src/main/java/com/windrose/coliseum/ludicoliseum/view/end/GameEndContract.kt
@@ -3,14 +3,9 @@ package com.windrose.coliseum.ludicoliseum.view.end
 interface GameEndContract {
     interface Presenter {
         fun start()
-        fun onRestart()
     }
 
     interface View {
         fun setContent(winners: List<String>)
-    }
-
-    interface Navigator {
-        fun goStartGame()
     }
 }
