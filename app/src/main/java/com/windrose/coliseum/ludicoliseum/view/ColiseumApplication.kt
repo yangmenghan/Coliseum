1546d12e0f94bcf4b99a93de959a29bf2571d825
Menghan Yang <yangmenghan@gmail.com>
2022-04-07 22:02:31 +0200

    Inject app module

--- app/src/main/java/com/windrose/coliseum/ludicoliseum/view/ColiseumApplication.kt
+++ app/src/main/java/com/windrose/coliseum/ludicoliseum/view/ColiseumApplication.kt
@@ -1,19 +1,27 @@
 package com.windrose.coliseum.ludicoliseum.view
 
-import com.windrose.coliseum.ludicoliseum.di.DaggerApplicationComponent
-import dagger.android.AndroidInjector
-import dagger.android.DaggerApplication
-import dagger.android.DispatchingAndroidInjector
-import dagger.android.HasAndroidInjector
-import javax.inject.Inject
+import android.app.Application
+import com.windrose.coliseum.ludicoliseum.view.end.di.GameEndModule
+import com.windrose.coliseum.ludicoliseum.view.game.di.GameModule
+import com.windrose.coliseum.ludicoliseum.view.start.di.StartGameModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.ksp.generated.module
 
-class ColiseumApplication : DaggerApplication(), HasAndroidInjector {
-    @Inject
-    lateinit var injector: DispatchingAndroidInjector<Any>
+class ColiseumApplication : Application() {
 
-    override fun androidInjector() = injector
-
-    override fun applicationInjector(): AndroidInjector<out DaggerApplication> =
-            DaggerApplicationComponent.factory().create(this)
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidLogger()
+            androidContext(this@ColiseumApplication)
+            modules(
+                GameEndModule().module,
+                StartGameModule().module,
+                GameModule().module,
+            )
+        }
+    }
 
 }
