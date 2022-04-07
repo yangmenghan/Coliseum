caad6c93e39f214268fd4ea286f8782585409945
Menghan Yang <yangmenghan@gmail.com>
2022-04-07 22:13:24 +0200

    Add missing modules

---
+++ app/src/main/java/com/windrose/coliseum/ludicoliseum/data/di/DataModule.kt
@@ -0,0 +1,8 @@
+package com.windrose.coliseum.ludicoliseum.data.di
+
+import org.koin.core.annotation.ComponentScan
+import org.koin.core.annotation.Module
+
+@Module
+@ComponentScan("com.windrose.coliseum.ludicoliseum.data")
+class DataModule
--- app/src/main/java/com/windrose/coliseum/ludicoliseum/view/ColiseumApplication.kt
+++ app/src/main/java/com/windrose/coliseum/ludicoliseum/view/ColiseumApplication.kt
@@ -1,6 +1,8 @@
 package com.windrose.coliseum.ludicoliseum.view
 
 import android.app.Application
+import com.windrose.coliseum.ludicoliseum.core.di.CoreModule
+import com.windrose.coliseum.ludicoliseum.data.di.DataModule
 import com.windrose.coliseum.ludicoliseum.view.end.di.GameEndModule
 import com.windrose.coliseum.ludicoliseum.view.game.di.GameModule
 import com.windrose.coliseum.ludicoliseum.view.start.di.StartGameModule
@@ -17,9 +19,11 @@ class ColiseumApplication : Application() {
             androidLogger()
             androidContext(this@ColiseumApplication)
             modules(
+                CoreModule().module,
+                DataModule().module,
+                GameModule().module,
                 GameEndModule().module,
                 StartGameModule().module,
-                GameModule().module,
             )
         }
     }
