8ddd25b6d65a719ad811b087312eff2ab4443d22
Myuu <yangmenghan@gmail.com>
2023-01-24 20:38:17 +0100

    Better handle game start button

--- app/src/main/java/games/windrose/coliseum/view/start/StartGameFragment.kt
+++ app/src/main/java/games/windrose/coliseum/view/start/StartGameFragment.kt
@@ -1,11 +1,13 @@
 package games.windrose.coliseum.view.start
 
 import android.os.Bundle
+import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import android.view.inputmethod.EditorInfo
 import android.widget.EditText
+import android.widget.ImageButton
 import androidx.fragment.app.Fragment
 import games.windrose.coliseum.R
 import games.windrose.coliseum.view.game.GameFragment
@@ -19,15 +21,28 @@ class StartGameFragment : Fragment(), StartGameContract.View {
     private val presenter: StartGamePresenter by inject { parametersOf(this) }
 
     private lateinit var playersNumberInput: EditText
-    private lateinit var createGameButton: Button
+    private lateinit var createGameButton: ImageButton
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_game_start, container, false).apply {
             playersNumberInput = findViewById(R.id.playersNumberInput)
+            playersNumberInput.setOnEditorActionListener { _, actionId, event ->
+                if (actionId == EditorInfo.IME_ACTION_DONE
+                    || event.action == KeyEvent.ACTION_DOWN
+                    && event.keyCode == KeyEvent.KEYCODE_ENTER) {
+                    onCreateGameClicked()
+                    true
+                } else {
+                    false
+                }
+            }
             createGameButton = findViewById(R.id.createGameButton)
-            createGameButton.setOnClickListener { presenter.onCreateGame(playersNumberInput.text.toString().toIntOrNull()) }
+            createGameButton.setOnClickListener { onCreateGameClicked() }
         }
     }
+
+    private fun onCreateGameClicked() =
+        presenter.onCreateGame(playersNumberInput.text.toString().toIntOrNull())
 
     override fun goToGame() {
         routeToFragment(GameFragment.newInstance())
